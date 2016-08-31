7be4e378f0f541c2e307d07f118d3bcfcae67308
Thomas B <thomas.bae0@gmail.com>
2016-08-31 15:34:17 -0400

    init script to load mongodb with some default data

---
+++ scripts/initmongodb.js
@@ -0,0 +1,7 @@
+
+var dbconn = new Mongo().getDB("bugdb");
+
+dbconn.bugs.remove({});
+
+dbconn.bugs.insert([{status:"open", priority:"P1", owner:"Tman", title:"App crashes upon opening5.3"},
+{status:"open", priority:"P2", owner:"TmanQ", title:"App crashes upon close"}]);
